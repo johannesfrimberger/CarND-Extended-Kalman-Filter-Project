6723bab86cef5bb2dec41b91a4869264c15cf777
johannesfrimberger <johannes.frimberger@gmail.com>
2017-03-26 11:07:32 +0200

    Enter all EKF formulas

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,6 @@
 #include "kalman_filter.h"
+#include "tools.h"
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -18,22 +20,49 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+    
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+    MatrixXd Hj = Tools::CalculateJacobian(x_);
+    
+    VectorXd z_pred(3);
+    z_pred[0] = sqrt(x_[0] * x_[0] + x_[1] * x_[1]);
+    z_pred[1] = atan2(x_[1], x_[0]);
+    if(z_pred[0] < 1e-4)
+        z_pred[2] = (x_[0] * x_[2] + x_[1] * x_[3]) / 1e-4;
+    else
+        z_pred[2] = (x_[0] * x_[2] + x_[1] * x_[3]) / z_pred[0];
+    
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = Hj.transpose();
+    
+    MatrixXd S = Hj * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+    
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * Hj) * P_;
 }
